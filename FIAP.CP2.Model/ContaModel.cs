7d92362f526adb6104204910050c14fceb21910b
joaoGFG <joaogabrielfg10@gmail.com>
2025-10-21 15:53:59 -0300

    testing

--- FIAP.CP2.Model/ContaModel.cs
+++ FIAP.CP2.Model/ContaModel.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace FIAP.CP2.Model
 {
-    internal class ContaModel
+    public class ContaModel
     {
+        public string? Id { get; set; }
+
+        public required string NomeTitular { get; set; }
+
+        public required string CPF { get; set; }
+
+        public required string TipoConta { get; set; }
+
+        public double Saldo { get; set; }
+
+        public DateTime DataAbertura { get; set; }
+
+        public bool Ativa { get; set; }
+
+        public string? NumeroConta { get; set; }
+
+        public ContaModel()
+        {
+            Id = Guid.NewGuid().ToString();
+            DataAbertura = DateTime.Now;
+            Saldo = 0;
+            Ativa = true;
+            NumeroConta = GerarNumeroConta();
+        }
+
+        private static string GerarNumeroConta()
+        {
+            var random = new Random();
+            return $"{random.Next(1000, 9999)}-{random.Next(0, 9)}";
+        }
     }
 }
--- FIAP.CP2.Model/TransacaoModel.cs
+++ FIAP.CP2.Model/TransacaoModel.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace FIAP.CP2.Model
 {
-    internal class TransacaoModel
+    public class TransacaoModel
     {
+        public string? Id { get; set; }
+
+        public required string ContaId { get; set; }
+
+        public required string Tipo { get; set; }
+
+        public double Valor { get; set; }
+
+        public DateTime DataHora { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public string? ContaDestinoId { get; set; }
+
+        public double SaldoAnterior { get; set; }
+
+        public double SaldoPosterior { get; set; }
+
+        public TransacaoModel()
+        {
+            Id = Guid.NewGuid().ToString();
+            DataHora = DateTime.Now;
+        }
     }
 }
