6050f8481b0855f5d78ce95e76cd6c6877d3cab8
joaoGFG <joaogabrielfg10@gmail.com>
2025-10-21 16:56:21 -0300

    feat: controllers

---
+++ FIAP.CP2.UI/Controllers/ContasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FIAP.CP2.Data;
+using FIAP.CP2.Model;
+
+namespace FIAP.CP2.UI.Controllers
+{
+    public class ContasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contas.ToListAsync());
+        }
+
+        // GET: Contas/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contaModel = await _context.Contas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contaModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contaModel);
+        }
+
+        // GET: Contas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contas/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NomeTitular,CPF,TipoConta,Saldo,DataAbertura,Ativa,NumeroConta")] ContaModel contaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contaModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contaModel);
+        }
+
+        // GET: Contas/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contaModel = await _context.Contas.FindAsync(id);
+            if (contaModel == null)
+            {
+                return NotFound();
+            }
+            return View(contaModel);
+        }
+
+        // POST: Contas/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,NomeTitular,CPF,TipoConta,Saldo,DataAbertura,Ativa,NumeroConta")] ContaModel contaModel)
+        {
+            if (id != contaModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contaModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContaModelExists(contaModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contaModel);
+        }
+
+        // GET: Contas/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contaModel = await _context.Contas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contaModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contaModel);
+        }
+
+        // POST: Contas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var contaModel = await _context.Contas.FindAsync(id);
+            if (contaModel != null)
+            {
+                _context.Contas.Remove(contaModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContaModelExists(string id)
+        {
+            return _context.Contas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FIAP.CP2.UI/Controllers/TransacoesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FIAP.CP2.Data;
+using FIAP.CP2.Model;
+
+namespace FIAP.CP2.UI.Controllers
+{
+    public class TransacoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransacoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transacoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transacoes.ToListAsync());
+        }
+
+        // GET: Transacoes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transacaoModel = await _context.Transacoes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (transacaoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transacaoModel);
+        }
+
+        // GET: Transacoes/Create
+        public IActionResult Create()
+        {
+            ViewData["ContaId"] = new SelectList(_context.Contas, "Id", "NomeTitular");
+            return View();
+        }
+
+        // POST: Transacoes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ContaId,Tipo,Valor,DataHora,Descricao,ContaDestinoId,SaldoAnterior,SaldoPosterior")] TransacaoModel transacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transacaoModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContaId"] = new SelectList(_context.Contas, "Id", "NomeTitular", transacaoModel.ContaId);
+            return View(transacaoModel);
+        }
+
+        // GET: Transacoes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transacaoModel = await _context.Transacoes.FindAsync(id);
+            if (transacaoModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContaId"] = new SelectList(_context.Contas, "Id", "NomeTitular", transacaoModel.ContaId);
+            return View(transacaoModel);
+        }
+
+        // POST: Transacoes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,ContaId,Tipo,Valor,DataHora,Descricao,ContaDestinoId,SaldoAnterior,SaldoPosterior")] TransacaoModel transacaoModel)
+        {
+            if (id != transacaoModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transacaoModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransacaoModelExists(transacaoModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContaId"] = new SelectList(_context.Contas, "Id", "NomeTitular", transacaoModel.ContaId);
+            return View(transacaoModel);
+        }
+
+        // GET: Transacoes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transacaoModel = await _context.Transacoes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (transacaoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transacaoModel);
+        }
+
+        // POST: Transacoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var transacaoModel = await _context.Transacoes.FindAsync(id);
+            if (transacaoModel != null)
+            {
+                _context.Transacoes.Remove(transacaoModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransacaoModelExists(string id)
+        {
+            return _context.Transacoes.Any(e => e.Id == id);
+        }
+    }
+}
