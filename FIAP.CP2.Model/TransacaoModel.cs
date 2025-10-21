8e940de3c7b9069b9ba61e0c53f0ddb6d9226260
joaoGFG <joaogabrielfg10@gmail.com>
2025-10-21 15:51:08 -0300

    feat: ContaModel and TransacaoModel for basic models

---
+++ FIAP.CP2.Model/TransacaoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP.CP2.Model
+{
+    internal class TransacaoModel
+    {
+    }
+}
