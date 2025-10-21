010eadf5f690016d24c27f2d2961f045d7dd6260
João <Joo@DESKTOP-KRF2E0G>
2025-10-21 15:07:10 -0300

    Adicionar arquivos de projeto.

---
+++ FIAP.CP2.Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace FIAP.CP2.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseOracle("User Id=rm559863;Password=110306;Data Source=oracle.fiap.com.br:1521/ORCL;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
